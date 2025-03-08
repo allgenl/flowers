119404ff5d77a36507f0c5fb46e1ead917b7cc00
Oleg Karnaukhov <allgenl@mail.ru>
2025-03-08 17:26:51 +0300

    Version 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -5,57 +5,19 @@ import styled from 'styled-components';
 
 const flowers = ['🌹', '🌷', '🌺', '🏵', '🌼', '🌻', '🌸', '🪻'];
 
-const questions = [
-  {
-    question: 'Выбери традицию',
-    options: [
-      { text: 'Смеёшься сам — рассмеши другого', flower: 'Подсолнух' },
-      { text: 'Покушал сам — покорми другого', flower: 'Роза' },
-      { text: 'Облюйся сам — облюй другого', flower: 'Лаванда' },
-    ],
-  },
-  {
-    question: 'Выбери цвет, который тебе нравится больше всего:',
-    options: [
-      { text: 'Желтый', flower: 'Подсолнух' },
-      { text: 'Фиолетовый', flower: 'Лаванда' },
-      { text: 'Красный', flower: 'Роза' },
-      { text: 'Розовый', flower: 'Тюльпан' },
-    ],
-  },
-  {
-    question: 'Как бы тебя описали друзья?',
-    options: [
-      { text: 'Энергичный и позитивный', flower: 'Подсолнух' },
-      { text: 'Спокойный и добрый', flower: 'Лаванда' },
-      { text: 'Чувственный и загадочный', flower: 'Роза' },
-      { text: 'Легкий на подъем и веселый', flower: 'Тюльпан' },
-    ],
-  },
-];
-
 const FlowerQuiz = () => {
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [questionsByUser, setQuestionsByUser] = useState(null);
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [answers, setAnswers] = useState([]);
   const [result, setResult] = useState(null);
 
-  const handleAnswer = (flower) => {
-    const newAnswers = [...answers, flower];
-    setAnswers(newAnswers);
-
-    if (currentQuestion + 1 < questions.length) {
-      setCurrentQuestion(currentQuestion + 1);
-    } else {
-      const flowerCounts = newAnswers.reduce((acc, flower) => {
-        acc[flower] = (acc[flower] || 0) + 1;
-        return acc;
-      }, {});
-
-      const mostCommonFlower = Object.keys(flowerCounts).reduce((a, b) => (flowerCounts[a] > flowerCounts[b] ? a : b));
-
-      setResult(mostCommonFlower);
-    }
-  };
+  useEffect(() => {
+    fetch('/users_questions.json')
+      .then((response) => response.json())
+      .then((data) => setQuestionsByUser(data))
+      .catch((error) => console.error('Ошибка загрузки JSON:', error));
+  }, []);
 
   const Petal = () => {
     const randomX = Math.random() * window.innerWidth;
@@ -79,88 +41,151 @@ const FlowerQuiz = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       setPetals((prev) => [...prev, <Petal key={Math.random()} />]);
-    }, 500);
+    }, 200);
 
     return () => clearInterval(interval);
   }, []);
 
-  return (
-    <StyledContainer>
-      {/* Контейнер для лепестков */}
-      <div className="absolute top-0 left-0 w-full h-full pointer-events-none">{petals}</div>
+  if (!questionsByUser)
+    return (
+      <StyledContainer>
+        <FlowersContainer className="absolute top-0 left-0 w-full h-full pointer-events-none">
+          {petals}
+        </FlowersContainer>
+        <div>Загрузка...</div>
+      </StyledContainer>
+    );
 
-      {/* Контейнер опроса */}
-      <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-        className="relative z-10"
-        style={{ margin: 'auto', zIndex: '10' }}
-      >
-        <Card
-          sx={{
-            backgroundColor: '#FFC0CB',
-            borderRadius: 4,
-            boxShadow: 3,
-            maxWidth: 400,
-            textAlign: 'center',
-            p: 2,
-          }}
-        >
+  if (!selectedUser) {
+    return (
+      <StyledContainer>
+        <FlowersContainer className="absolute top-0 left-0 w-full h-full pointer-events-none">
+          {petals}
+        </FlowersContainer>
+
+        <Card sx={{ p: 2, textAlign: 'center' }}>
           <CardContent>
-            {result ? (
-              <>
-                <Typography variant="h5" fontWeight="bold">
-                  Ты — {result}! 🌸
-                </Typography>
-                <Typography variant="body1" sx={{ mt: 2 }}>
-                  Этот цветок отражает твою личность и энергию!
-                </Typography>
-                <Button
-                  variant="contained"
-                  sx={{ mt: 3, backgroundColor: '#E91E63' }}
-                  onClick={() => {
-                    setCurrentQuestion(0);
-                    setAnswers([]);
-                    setResult(null);
-                  }}
-                >
-                  Пройти снова
-                </Button>
-              </>
-            ) : (
-              <>
-                <Typography variant="h6" fontWeight="bold">
-                  {questions[currentQuestion].question}
-                </Typography>
-                <div style={{ marginTop: '1rem' }}>
-                  {questions[currentQuestion].options.map((option, index) => (
-                    <Button
-                      key={index}
-                      variant="contained"
-                      fullWidth
-                      sx={{ mt: 1, backgroundColor: '#F48FB1' }}
-                      onClick={() => handleAnswer(option.flower)}
-                    >
-                      {option.text}
-                    </Button>
-                  ))}
-                </div>
-              </>
-            )}
+            <Typography variant="h4">Какой ты цветочек?</Typography>
+            <Typography variant="h6">Выбери себя:</Typography>
+            <StyledChoose>
+              {Object.entries(questionsByUser).map((user, userQuestions) => {
+                return (
+                  <StyledButton
+                    className="choose"
+                    color="secondary"
+                    key={user[0]}
+                    onClick={() => setSelectedUser(user[0])}
+                    sx={{ mt: 1 }}
+                  >
+                    {user[0]}
+                  </StyledButton>
+                );
+              })}
+            </StyledChoose>
           </CardContent>
         </Card>
-      </motion.div>
+      </StyledContainer>
+    );
+  }
+
+  const questions = questionsByUser[selectedUser].questions || [];
+
+  const handleAnswer = (flower) => {
+    const newAnswers = [...answers, flower];
+    setAnswers(newAnswers);
+    if (currentQuestion + 1 < questions.length) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      setResult(questionsByUser[selectedUser].flower);
+    }
+  };
+
+  return (
+    <StyledContainer>
+      <FlowersContainer className="absolute top-0 left-0 w-full h-full pointer-events-none">{petals}</FlowersContainer>
+      <Card sx={{ p: 2, textAlign: 'center', width: '100%', maxWidth: '400px' }}>
+        <CardContent>
+          {result ? (
+            <>
+              <StyledAnswers>
+                <Typography variant="h5">Ты — {result}!</Typography>
+                <StyledPhoto>
+                  <img src={`/${questionsByUser[selectedUser].photo}`} alt="" />
+                </StyledPhoto>
+                <StyledButton color="secondary">Смотреть поздравление</StyledButton>
+              </StyledAnswers>
+            </>
+          ) : (
+            <>
+              <Typography variant="h6">{questions[currentQuestion].question}</Typography>
+              <StyledAnswers>
+                {questions[currentQuestion].options.map((option, index) => (
+                  <StyledButton
+                    color="secondary"
+                    key={`${currentQuestion}-${index}`}
+                    onClick={() => handleAnswer(option.flower)}
+                    sx={{ mt: 1 }}
+                  >
+                    {option.text}
+                  </StyledButton>
+                ))}
+              </StyledAnswers>
+            </>
+          )}
+        </CardContent>
+      </Card>
     </StyledContainer>
   );
 };
 
 export default FlowerQuiz;
 
-const StyledContainer = styled(Stack)(() => ({
+const FlowersContainer = styled('div')(() => ({
+  position: 'absolute',
+  top: '0',
+  left: '0',
+  width: '100vw',
+  height: '100vh',
+  pointerEvents: 'none',
+}));
+
+const StyledContainer = styled('div')(() => ({
   width: '100vw',
   height: '100vh',
   display: 'flex',
-  margin: '0 auto',
-  zIndex: '10',
+  justifyContent: 'center',
+  alignItems: 'center',
+  position: 'relative',
+}));
+
+const StyledAnswers = styled('div')(() => ({
+  flexDirection: 'column',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  position: 'relative',
+}));
+
+const StyledButton = styled(Button)(() => ({
+  width: '100%',
+  backgroundColor: '#FFC0CB',
+  color: 'white',
+}));
+const StyledPhoto = styled('div')(() => ({
+  width: '100%',
+  margin: '1rem 0',
+  '& img': {
+    borderRadius: '1rem',
+    width: '100%',
+    maxHeight: '80vh',
+    objectFit: 'cover',
+  },
+}));
+const StyledChoose = styled('div')(() => ({
+  display: 'grid',
+  gridTemplateColumns: '1fr 1fr',
+  gap: '8px',
+  '&  .choose': {
+    fontSize: '12px !important',
+  },
 }));
